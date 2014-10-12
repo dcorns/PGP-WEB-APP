3ef677029196a58aaf44f508d834e7ed4db5d538
Dale Corns <drcorns@hotmail.com>
2014-10-12 01:06:10 -0700

    use student to select survey

--- app/js/ui.js
+++ app/js/ui.js
@@ -4,9 +4,9 @@
 'use strict';
 module.exports = function(){
   return {
-    fillInput: function(id, val){
+    fillInput: function (id, val) {
       if (document.getElementById(id)) {
-        console.log(id+','+val);
+        console.log(id + ',' + val);
         document.getElementById(id).value = val || "";
         return this;
       }
