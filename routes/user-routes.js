b790c545cca52976d1e64ad4c928b9c995c71073
Dale Corns <drcorns@hotmail.com>
2014-10-22 23:58:46 -0700

    Add server side validation of password, first and last name

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -19,7 +19,11 @@ module.exports = function (app) {
   app.post(baseUrl, function (req, res) {
     var user = new User(req.body);
     user.email = user.email.toLowerCase();
+    //validate input
     if(!(validate.isEmail(user.email))) return res.status(505).json({error:'Not a valid email address!'});
+    if(!(validate.isLength(user.password, 6) )) return res.status(506).json({error:'Password must be at least 6 characters long'});
+    if(!(validate.isLength(user.lastName, 2))) return res.status(507).json({error:'Last name must be at least 2 characters long'});
+    if(!(validate.isLength(user.firstName, 2))) return res.status(508).json({error:'First name must be at least 2 characters long'});
     user.roll = 'student';
     var a = auth(user);
     a.encrypt(function (usr) {
