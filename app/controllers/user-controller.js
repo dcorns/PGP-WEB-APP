047bd645fa09e8b117919d1806db452ee2613a1d
Dale Corns <drcorns@hotmail.com>
2014-11-20 17:31:04 -0800

    comment all posible future use code from user-controller

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -10,19 +10,6 @@ module.exports = function(app) {
     var ux = ui();
     ux.hideMainButtons();
 
-    $scope.getAllUsers = function() {
-      $http({
-        method: 'GET',
-        url: '/api/v_0_0_1/users'
-      }).success(function(data) {
-        $scope.users = data;
-        $scope.selectedUser = $scope.users[0];
-      }).error(function(data, status) {
-        console.log('error!');
-        console.log(status);
-      });
-    };
-
     $scope.saveNewUser = function() {
       $http.post('api/v_0_0_1/users', $scope.newUser)
         .success(function(data) {
@@ -75,33 +62,46 @@ module.exports = function(app) {
     };
 
 
-    $scope.editUser = function(user) {
-      user.editing = true;
-    };
+    //$scope.getAllUsers = function() {
+    //  $http({
+    //    method: 'GET',
+    //    url: '/api/v_0_0_1/users'
+    //  }).success(function(data) {
+    //    $scope.users = data;
+    //    $scope.selectedUser = $scope.users[0];
+    //  }).error(function(data, status) {
+    //    console.log('error!');
+    //    console.log(status);
+    //  });
+    //};
 
-    $scope.saveUser = function(user) {
-      user.editing = null;
-      $http.put('api/v_0_0_1/users/' + user._id, user)
-        .success(function(data) {
-          $scope.getAllUsers();
-        })
-        .error(function(data) {
-          console.log(data);
-        });
-    };
+    //$scope.editUser = function(user) {
+    //  user.editing = true;
+    //};
 
-    $scope.deleteUser = function(user) {
-      $http.delete('api/v_0_0_1/users/' + user._id, user)
-        .success(function(data) {
-          $scope.getAllUsers();
-        })
-        .error(function(data) {
-          console.log(data);
-        });
-    };
+    //$scope.saveUser = function(user) {
+    //  user.editing = null;
+    //  $http.put('api/v_0_0_1/users/' + user._id, user)
+    //    .success(function(data) {
+    //      $scope.getAllUsers();
+    //    })
+    //    .error(function(data) {
+    //      console.log(data);
+    //    });
+    //};
 
-    $scope.deleteAll = function() {
-      $scope.users.forEach(function(user) {$scope.deleteUser(user)});
-    }
+    //$scope.deleteUser = function(user) {
+    //  $http.delete('api/v_0_0_1/users/' + user._id, user)
+    //    .success(function(data) {
+    //      $scope.getAllUsers();
+    //    })
+    //    .error(function(data) {
+    //      console.log(data);
+    //    });
+    //};
+
+  //  $scope.deleteAll = function() {
+  //    $scope.users.forEach(function(user) {$scope.deleteUser(user)});
+  //  }
   });
 };
