8646cdc48a9bd92db403ba85214039f420c11a3a
Dale Corns <drcorns@hotmail.com>
2014-12-08 17:40:04 -0800

    match createpgp rating elements with viewpgp close#35

--- test/mocha/api/pgp-routes-test.js
+++ test/mocha/api/pgp-routes-test.js
@@ -2,11 +2,11 @@
  * Created by dcorns on 12/7/14.
  */
 'use strict';
-var Note = require('../api/models/note');
-var User = require('../api/models/user');
-var auth = require('../api/js/authorize');
-var db = require('../api/js/dbutils');
-var corngoose = require('../api/js/corngoose');
+var Note = require('../../../api/models/note');
+var User = require('../../../api/models/user');
+var auth = require('../../../api/js/authorize');
+var db = require('../../../api/js/dbutils');
+var corngoose = require('../../../api/js/corngoose');
 var chai = require('chai');
 var chaihttp = require('chai-http');
 chai.use(chaihttp);
@@ -15,14 +15,11 @@ var expect = chai.expect;
 describe('get responses for /api/v_0_0_1/pgps', function(){
   chai.request('http://localhost:3000')
     .get('/api/v_0_0_1/pgps')
-    .req(function (req) {
-      req.send();
-    })
-    .res(function (res) {
-      expect(res).to.have.status(400);
-      expect(res.body).to.have.property('email');
-      expect(res.body.email).to.eql('Not a valid email address!');
-      done();
+    .set('authorization','testToken')
+    .send()
+    .end(function(err, res){
+      expect(token).to.eql('testToken');
     });
+
 });
 
