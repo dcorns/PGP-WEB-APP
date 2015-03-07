0b24d6d5f080115bf7413ad0a9e184c52800835c
Dale Corns <drcorns@hotmail.com>
2015-03-06 18:34:49 -0800

    replace angular routes with switch statement

--- app/js/app.js
+++ app/js/app.js
@@ -11,6 +11,51 @@ require('../controllers/survey-controller')(pgpApp);
 require('../controllers/viewPGP-controller')(pgpApp);
 require('../directives/drcOptCls-Directive')(pgpApp);
 
+switch(dgRoute) {
+  case '/student_survey':
+    return {
+      templateUrl: 'views/student_survey.html',
+      controller: 'surveyController'
+    };
+    break;
+  case '/create_PGP':
+    return {
+      templateUrl: 'views/create_PGP.html',
+      controller: 'pgpsController'
+    };
+    break;
+  case '/preview_PGP':
+    return {
+      templateUrl: 'views/preview_PGP.html',
+      controller: 'pgpsController'
+    };
+    break;
+  case '/view_PGP':
+    return {
+      templateUrl: 'views/view_PGP.html',
+      controller: 'viewPGPController'
+    };
+    break;
+  case '/create_Account':
+    return {
+      templateUrl: 'views/create_Account.html',
+      controller: 'userController'
+    };
+    break;
+  case '/login':
+    return {
+      templateUrl: 'views/login.html',
+      controller: 'userController'
+    };
+    break;
+  default:
+    return {
+      templateUrl: 'views/home.html',
+      controller: 'userController'
+    };
+    break;
+}
+
 pgpApp.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     .when('/student_survey', {
