f21d0a78c938a47dbaaf8cdd47c3bfe324a43a37
Dale Corns <drcorns@hotmail.com>
2015-03-12 14:40:33 -0700

    refactor, add app object

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,10 @@
 'use strict';
-//comment for changes
-
-var rp = require('../js/dgRouteProvider');
-
-var surveyCtrl = require('../controllers/surveyController');
-var pgpsCtrl = require('../controllers/pgpsController');
-var userCtrl = require('../controllers/userController');
-var viewPGPCtrl = require('../controllers/viewPGPController');
+var dgApp = {};
+dgApp.userCtrl = require('../controllers/userController');
+dgApp.surveyCtrl = require('../controllers/surveyController');
+dgApp.pgpsCtrl = require('../controllers/pgpsController');
+dgApp.viewPGPCtrl = require('../controllers/viewPGPController');
+require('../js/dgRouteProvider')(dgApp); //adds loadRoute method to dgApp
 
 function startdgApp(startup){
   var preOnload = window.onload;
@@ -25,13 +23,13 @@ function firstDo(){
   //Handle Refresh by checking session storage for last href and redirecting if it exists
   var lastHref = window.sessionStorage.getItem('href');
   if (lastHref){
-    postOffice(lastHref);
+    dgApp.loadRoute(lastHref);
   }
   else{//load home template
     lastHref = '#/home';
     window.sessionStorage.setItem('href', lastHref);
     window.history.pushState(null, null, lastHref);
-    postOffice(lastHref);
+    dgApp.loadRoute(lastHref);
   }
   //Add event handlers for 'a' tags
   var links = document.getElementsByTagName('a');
@@ -41,70 +39,15 @@ function firstDo(){
       window.sessionStorage.setItem('href', this.href);
       window.history.pushState(null, null, this.href);
       e.preventDefault();
-      postOffice(this.href);
+      dgApp.loadRoute(this.href);
     });
   }
   //Add front and back button handler
   window.addEventListener('popstate', function(){
     window.sessionStorage.setItem('href', location.href);
-    postOffice(location.href);
+    dgApp.loadRoute(location.href);
   });
 
-}
-
-function postOffice(dgRoute){
-  //grab the '#' and everything that follows it.
-  dgRoute = dgRoute.substr(dgRoute.indexOf('#'));
-  switch(dgRoute) {
-    case '#/student_survey':
-      return rp({
-        templateUrl: 'views/student_survey.html',
-        controller: surveyCtrl
-      });
-      break;
-    case '#/create_PGP':
-      return rp({
-        templateUrl: 'views/create_PGP.html',
-        controller: pgpsCtrl
-      });
-      break;
-    case '#/preview_PGP':
-      return rp({
-        templateUrl: 'views/preview_PGP.html',
-        controller: pgpsCtrl
-      });
-      break;
-    case '#/view_PGP':
-      return rp({
-        templateUrl: 'views/view_PGP.html',
-        controller: viewPGPCtrl
-      });
-      break;
-    case '#/create_Account':
-      return rp({
-        templateUrl: 'views/create_Account.html',
-        controller: userCtrl
-      });
-      break;
-    case '#/login':
-      return rp({
-        templateUrl: 'views/login.html',
-        controller: userCtrl
-      });
-      break;
-    case '#/home':
-      return rp({
-        templateUrl: 'views/home.html',
-        controller: userCtrl
-      });
-      break;
-    default:
-      return rp({
-        templateUrl: 'views/home.html',
-        controller: userCtrl
-      });
-      break;
-  }
 }
 
 startdgApp(firstDo);
--- app/js/dgRouteProvider.js
+++ app/js/dgRouteProvider.js
@@ -1,10 +1,70 @@
 /**
  * dgRouteProvider
  * Created by dcorns on 3/6/15.
+ * Handles client routing and the script/controller that is used for each route
  */
 'use strict';
 
-module.exports = function(rtObj){
+function loadRoute(rtObj){
   document.getElementById("dgView").innerHTML='<object type="text/html" data=' + rtObj.templateUrl + '></object>';
   rtObj.controller();
+}
+
+module.exports = function(app){
+  app.loadRoute = function(dgRoute){
+    //grab the '#' and everything that follows it.
+    dgRoute = dgRoute.substr(dgRoute.indexOf('#'));
+    switch(dgRoute) {
+      case '#/student_survey':
+        return loadRoute({
+          templateUrl: 'views/student_survey.html',
+          controller: app.surveyCtrl
+        });
+        break;
+      case '#/create_PGP':
+        return loadRoute({
+          templateUrl: 'views/create_PGP.html',
+          controller: app.pgpsCtrl
+        });
+        break;
+      case '#/preview_PGP':
+        return loadRoute({
+          templateUrl: 'views/preview_PGP.html',
+          controller: app.pgpsCtrl
+        });
+        break;
+      case '#/view_PGP':
+        return loadRoute({
+          templateUrl: 'views/view_PGP.html',
+          controller: app.viewPGPCtrl
+        });
+        break;
+      case '#/create_Account':
+        return loadRoute({
+          templateUrl: 'views/create_Account.html',
+          controller: app.userCtrl
+        });
+        break;
+      case '#/login':
+        return loadRoute({
+          templateUrl: 'views/login.html',
+          controller: app.userCtrl
+        });
+        break;
+      case '#/home':
+        return loadRoute({
+          templateUrl: 'views/home.html',
+          controller: app.userCtrl
+        });
+        break;
+      default:
+        return loadRoute({
+          templateUrl: 'views/home.html',
+          controller: app.userCtrl
+        });
+        break;
+    }
+
+};
+
 };
