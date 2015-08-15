6a7bbd94a79173c3d80d2286ac877d0faaf8ae12
Dale Corns <drcorns@hotmail.com>
2015-08-15 10:55:23 -0700

    use gulp

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('webpack', function(){
       output:{
         filename: 'bundle.js'
       }
-    })
+    }))
       .pipe(gulp.dest('./build'));
 });
 
@@ -38,4 +38,23 @@ gulp.task('webpackTests', function(){
     .pipe(gulp.dest('./app/test'));
 });
 
+gulp.task('watch', function(){
+  gulp.watch('app/**/*.js', ['webpack']);
+  gulp.watch('sass/**/*.sass', ['sass']);
+});
 
+gulp.task('sass', function(){
+return sass(glob.sync('sass/**/*.sass'))
+  .pipe(gulp.dest('./build/css/'))
+});
+
+gulp.task('angularTest', ['webpackTests'], function(){
+return gulp.src(['test/testmain.js'])
+  .pipe(karma({
+    configfile: 'karma.conf.js',
+    action: 'run'
+  }));
+});
+
+gulp.task('default', ['connect', 'watch']);
+gulp.task('unitTests', ['webpackTests', 'angularTests']);
