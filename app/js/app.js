2d3785bdc018eb4b71f06221b6f4a9a5b7c14e1a
Dale Corns <drcorns@hotmail.com>
2015-08-15 23:52:17 -0700

    add stripped down angular

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,6 @@
 'use strict';
 var angular = require('angular');
 var bannerController = require('./controllers/bannerController');
-var pgpApp = angular.module('pgpApp', [])
-  .controller('firstController', function($scope){
-  $scope.testthis = 'testthis';
-});
+var pgpApp = angular.module('pgpApp', []);
+
 pgpApp.controller('bannerController', ['$scope', bannerController]);
